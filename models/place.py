909933d1fb4b1da390efb45a87a00fdb22603cb1
Terry Wambui <shikuwambui@gmail.com>
2023-09-10 01:07:27 +0300

    temp

--- models/place.py
+++ models/place.py
@@ -20,6 +20,7 @@ place_amenity = Table('place_amenity', Base.metadata,
 
 
 class Place(BaseModel, Base):
+
     """ A place to stay """
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
