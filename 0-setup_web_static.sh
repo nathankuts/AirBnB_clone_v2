7aa4311b42236ac8ef75ea730a7fd393d15100a4
Terry wambui <shikuwambui@gmail.com>
2023-09-10 14:48:31 +0000

    editing

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,8 +9,14 @@ sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 
-# Craete a fake HTML file for testing
-echo "Hello, web_static!" | sudo tee /data/web_static/releases/test/index.html
+# Create a fake HTML file for testing
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" >> /data/web_static/releases/test/index.html
 
 # Create & recreate a symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
