4e841b374179c56e873b1366b1793fe9de820e1d
Terry Wambui <shikuwambui@gmail.com>
2023-09-22 17:20:39 +0300

    Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,11 +9,11 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Displays a HTML page with a list of all states"""
-    states = storage.all("State")
-    return render_template('7-states_list.html', states=states)
+@app.route('/cities_by_state', strict_slashes=False)
+def cities_by_state():
+    """Displays a HTML page with a list of all states & cities"""
+    states = storage.all(State)
+    return render_template('8-states_list.html', states=states)
 
 
 @app.teardown_appcontext
