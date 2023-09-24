e04b086479fd45f8a8a3dbde6bd76e9108b5d39e
Terry Wambui <shikuwambui@gmail.com>
2023-09-25 00:56:15 +0300

    fixed some errors

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -3,6 +3,9 @@
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
 
 
 app = Flask(__name__)
@@ -17,9 +20,9 @@ def teardown(self):
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_filters():
     """Displays HBnB filters page"""
-    states = storage.all("State")
-    amenities = storage.all("Amenity")
-    places = storage.all("Place")
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    places = storage.all(Place)
     return render_template("100-hbnb.html",
                            states=states, amenities=amenities, places=places)
 
